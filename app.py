a8a9fe0f0fa5245e0a361ef7c4222dfe5ddec173
tdsatish2402 <tdsatish2402@gmail.com>
2026-06-11 11:32:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title("Trade Concerns Dashboard")
 
-df = pd.read_excel("TrCM_Master_Database.xlsx")
+df = pd.read_excel("TrCM_Master_Database_v2.xlsx")
 
 st.write("Number of records:", len(df))
 
